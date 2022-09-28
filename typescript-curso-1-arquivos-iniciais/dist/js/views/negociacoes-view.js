620056263f340cfb586afde1dae6fa80ed81bfaa
Lucas W. da Motta <lucaswantz@hotmail.com>
2022-09-28 09:04:56 -0300

    Typescript: Curso 2 - Aula 3

--- typescript-curso-1-arquivos-iniciais/dist/js/views/negociacoes-view.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/views/negociacoes-view.js
@@ -14,7 +14,7 @@ export class NegociacoesView extends View {
 				${model.lista().map(negociacao => {
             return `
 						<tr>
-							<td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+							<td>${this.formatar(negociacao.data)}</td>
 							<td>${negociacao.quantidade}</td>
 							<td>${negociacao.valor}</td>
 						</tr>
@@ -22,5 +22,8 @@ export class NegociacoesView extends View {
         }).join('')}
 			</tbody>
 		</table>`;
+    }
+    formatar(data) {
+        return new Intl.DateTimeFormat().format(data);
     }
 }
